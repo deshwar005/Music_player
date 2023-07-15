a53b101310a406edb2829ad5f469f1515a0d6a42
gunadeshwar.a <gunadeshwar.a@gofrugal.com>
2023-07-15 13:36:41 +0530

    al

--- src/Components/Bottom.js
+++ src/Components/Bottom.js
@@ -2,7 +2,7 @@ import React , {useEffect} from 'react';
 import ReactAudioPlayer from 'react-audio-player';
 import './Bottom.css';
 import { useSelector } from 'react-redux';
-function Bottom(props) {
+function Bottom() {
     const songUrl = useSelector((state) => state.songUrl);
     useEffect(()=>{
         console.log(songUrl);
