69770f9974afcee7e6c2898e746eae2aa169cab5
gunadeshwar.a <gunadeshwar.a@gofrugal.com>
2023-08-25 18:37:51 +0530

    a

--- src/Components/Sidemusic.js
+++ src/Components/Sidemusic.js
@@ -30,14 +30,14 @@ function Sidemusic({ img, songname, song, title }) {
             <div className='playbutton'>
                 {/* <img src={songstate &&  title_playing===title  ? pause:play_img}alt="" ></img> */}
 
-                {songstate && title_playing === title ? (<svg xmlns="http://www.w3.org/2000/svg" width="100%" height="40" fill="red" class="bi bi-pause-circle-fill" viewBox="0 0 16 16"  onClick={() => {
+                {songstate && title_playing === title ? (<svg xmlns="http://www.w3.org/2000/svg" width="100%" height="40" fill="red" className="bi bi-pause-circle-fill" viewBox="0 0 16 16"  onClick={() => {
                         pausesong(song, title);
                       
                     }}>
   <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM6.25 5C5.56 5 5 5.56 5 6.25v3.5a1.25 1.25 0 1 0 2.5 0v-3.5C7.5 5.56 6.94 5 6.25 5zm3.5 0c-.69 0-1.25.56-1.25 1.25v3.5a1.25 1.25 0 1 0 2.5 0v-3.5C11 5.56 10.44 5 9.75 5z"/>
 </svg>)
                     :
-                    (<svg xmlns="http://www.w3.org/2000/svg" width="100%" height="40" fill="red" class="bi bi-play-circle-fill" viewBox="0 0 16 16" onClick={() => {
+                    (<svg xmlns="http://www.w3.org/2000/svg" width="100%" height="40" fill="red" className="bi bi-play-circle-fill" viewBox="0 0 16 16" onClick={() => {
                         playsong(song, title);
                      
                     }}>
